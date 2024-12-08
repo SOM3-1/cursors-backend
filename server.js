ea94d767edf15a5c0f6e70d1fd7dcc5147c88b02
SOM3-1 <dush.gowda@gmail.com>
2024-12-07 20:31:37 -0600

    azure

--- server.js
+++ server.js
@@ -35,23 +35,40 @@ const cursorImages = Array.from({ length: 19 }, (_, i) => `${baseURL}/${i + 1}.s
 
 const cursors = {};
 
-const getUserLocation = async () => {
+// const getUserLocation = async () => {
+//   try {
+//     const response = await axios.get("http://ip-api.com/json");
+//     const { country, countryCode } = response.data;
+//     const flag = `https://flagcdn.com/w40/${countryCode.toLowerCase()}.png`;
+//     return { country, flag };
+//   } catch (error) {
+//     console.error("Error fetching location:", error);
+//     return { country: "Unknown", flag: "" }; 
+//   }
+// };
+
+const getUserLocation = async (ip) => {
   try {
-    const response = await axios.get("http://ip-api.com/json");
-    const { country, countryCode } = response.data;
-    const flag = `https://flagcdn.com/w40/${countryCode.toLowerCase()}.png`;
-    return { country, flag };
+    const response = await axios.get(`https://geolocation-db.com/json/`);
+    const { country_name, country_code } = response.data;
+
+    const flag = country_code ? `https://flagcdn.com/w40/${country_code.toLowerCase()}.png` : "";
+
+    return { country: country_name || "Unknown", flag };
   } catch (error) {
     console.error("Error fetching location:", error);
-    return { country: "Unknown", flag: "" }; 
+    return { country: "Unknown", flag: "" };
   }
 };
+
 
 io.on("connection", async (socket) => {
   console.log(`User connected: ${socket.id}`);
 
   const randomCursorImage = cursorImages[Math.floor(Math.random() * cursorImages.length)];
-  const locationData = await getUserLocation();
+  const forwardedFor = socket.handshake.headers["x-forwarded-for"];
+  const clientIp = forwardedFor ? forwardedFor.split(",")[0] : socket.handshake.address;
+  const locationData = await getUserLocation(clientIp);
 
   cursors[socket.id] = {
     name: "",
